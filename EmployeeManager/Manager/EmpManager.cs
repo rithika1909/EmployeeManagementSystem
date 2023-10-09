ea9e10bbca41bc480fe2ef222ad54f60064e7fb6
Rithika L <rithika.l@tvsnext.com>
2023-10-09 15:36:24 +0530

    [Rithika]Added

---
+++ EmployeeManager/Manager/EmpManager.cs
@@ -0,0 +1,71 @@
+﻿using EmployeeManager.IManager;
+using EmployeeModels;
+using EmployeeRepository.IEmpRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace EmployeeManager.Manager
+{
+    public class EmployeeManager : IEmpManager
+    {
+        public readonly IEmpRepo empRepo;
+        public EmployeeManager(IEmpRepo empRepo)
+        {
+            this.empRepo = empRepo;
+        }
+        public Employee AddEmployee(Employee obj)
+        {
+            try
+            {
+                var result = this.empRepo.AddEmployee(obj);
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public bool DeleteEmployee(int empId)
+        {
+            try
+            {
+                var result = this.empRepo.DeleteEmployee(empId);
+                return result;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public List<Employee> GetAllEmployee()
+        {
+            try
+            {
+                var result = this.empRepo.GetAllEmployee();
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public bool UpdateEmployee(Employee obj)
+        {
+            try
+            {
+                var result = this.empRepo.UpdateEmployee(obj);
+                return result;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
